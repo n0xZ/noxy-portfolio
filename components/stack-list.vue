eefc3085dc5a0aaa136a3ba413498cdc9409a74e
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-11 16:07:08 -0300

    setup stack list component

---
+++ components/stack-list.vue
@@ -0,0 +1,63 @@
+<template>
+	<aside
+		class="grid xl:grid-cols-3 lg:grid-cols-3 grid-cols-2 place-items-center gap-8 container mx-auto w-full max-w-xl"
+	>
+		<div
+			class="flex flex-col justify-center items-center space-y-3 border-2 rounded-lg border-rose-300 dark:border-rose-900 p-7 w-28 hover:opacity-70 duration-150 ease-in-out"
+		>
+			<IconStackTypescript />
+			<span class="dark:c-neutral-200">Typescript</span>
+		</div>
+
+		<div
+			class="flex flex-col justify-center items-center space-y-3 border-2 rounded-lg border-rose-300 dark:border-rose-900 p-7 w-28 hover:opacity-70 duration-150 ease-in-out"
+		>
+			<IconStackReact />
+			<span class="dark:c-neutral-200">React</span>
+		</div>
+		<div
+			class="flex flex-col justify-center items-center space-y-3 border-2 rounded-lg border-rose-300 dark:border-rose-900 p-7 w-28 hover:opacity-70 duration-150 ease-in-out"
+		>
+			<IconStackVue />
+			<span class="dark:c-neutral-200">Vue</span>
+		</div>
+
+		<div
+			class="flex flex-col justify-center items-center space-y-3 border-2 rounded-lg border-rose-300 dark:border-rose-900 p-7 w-28 hover:opacity-70 duration-150 ease-in-out"
+		>
+			<IconStackNode />
+			<span class="dark:c-neutral-200">Node</span>
+		</div>
+		<div
+			class="flex flex-col justify-center items-center space-y-3 border-2 rounded-lg border-rose-300 dark:border-rose-900 p-7 w-28 hover:opacity-70 duration-150 ease-in-out"
+		>
+			<IconStackNext />
+			<span class="dark:c-neutral-200">NextJS</span>
+		</div>
+		<div
+			class="flex flex-col justify-center items-center space-y-3 border-2 rounded-lg border-rose-300 dark:border-rose-900 p-7 w-28 hover:opacity-70 duration-150 ease-in-out"
+		>
+			<IconStackRemix />
+			<span class="dark:c-neutral-200">Remix</span>
+		</div>
+
+		<div
+			class="flex flex-col justify-center items-center space-y-3 border-2 rounded-lg border-rose-300 dark:border-rose-900 p-7 w-28 hover:opacity-70 duration-150 ease-in-out"
+		>
+			<IconStackPrisma />
+			<span class="dark:c-neutral-200">Prisma</span>
+		</div>
+		<div
+			class="flex flex-col justify-center items-center space-y-3 border-2 rounded-lg border-rose-300 dark:border-rose-900 p-7 w-28 hover:opacity-70 duration-150 ease-in-out"
+		>
+			<IconStackSolid />
+			<span class="dark:c-neutral-200">SolidJS</span>
+		</div>
+		<div
+			class="flex flex-col justify-center items-center space-y-3 border-2 rounded-lg border-rose-300 dark:border-rose-900 p-7 w-28 hover:opacity-70 duration-150 ease-in-out"
+		>
+			<IconStackAstro />
+			<span class="dark:c-neutral-200">Astro</span>
+		</div>
+	</aside>
+</template>
