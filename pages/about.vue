54ee5ace0530629ad00d818864b936bee5ae3a4e
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-11 16:12:58 -0300

    chore: setup about me page

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+	useHead({ title: 'Gonzalo Molina - About me' })
+</script>
+<template>
+	<section
+		class="h-full flex flex-col items-center space-y-4 container mx-auto max-w-xl p-5"
+	>
+		<h2 class="text-center text-4xl c-rose-500" title="About me">About me</h2>
+		<article class="flex flex-col justify-center space-y-3 p-5 h-full mb-5">
+			<p>
+				I'm 21 years old and I'm a software developer based in Argentina. I've been
+				studying programming since I was at high school, and I'm a big fan of it!
+			</p>
+			<p>My actual stack is based on these technologies:</p>
+		</article>
+		<StackList />
+	</section>
+</template>
