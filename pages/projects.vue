7be5cb9b6ee292863b888cec081fcbf38c260c1b
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-11 16:07:51 -0300

    chore: add project page

---
+++ pages/projects.vue
@@ -0,0 +1,15 @@
+<script  setup lang="ts">
+
+import { getProjects } from '~~/utils/projects';
+const actualProjects = getProjects()
+useHead({ title: 'Gonzalo Molina - My projects' })
+</script>
+<template>
+	<section
+		class="min-h-screen h-full flex flex-col justify-center items-center space-y-3 container mx-auto max-w-3xl"
+		id="projects"
+	>
+		<h4 class="text-center text-4xl c-rose-500" title="Projects">My projects</h4>
+		<ProjectList :projects="actualProjects" />
+	</section>
+</template>
