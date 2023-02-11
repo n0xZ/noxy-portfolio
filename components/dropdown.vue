e7bfd643f01b57307bd086508c03bfb7f47ef089
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-02-11 15:29:10 -0300

    setup header with mobile dropdown

---
+++ components/dropdown.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+	import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+	type Props = {
+		toggleDarkMode: () => boolean
+	}
+	const props = defineProps<Props>()
+</script>
+
+<template>
+	<Menu as="div" className=" relative xl:hidden lg:hidden  ">
+		<MenuButton
+			class="w-full flex flex-row justify-center items-center space-x-2 rounded-md text-sm font-medium c-neutral-500 dark:c-neutral-50 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+		>
+			<IconMenu />
+		</MenuButton>
+		<transition
+			enter="transition ease-out duration-100"
+			enterFrom="transform opacity-0 scale-95"
+			enterTo="transform opacity-100 scale-100"
+			leave="transition ease-in duration-75"
+			leaveFrom="transform opacity-100 scale-100"
+			leaveTo="transform opacity-0 scale-95"
+		>
+			<MenuItems
+				className="absolute right-0 mt-2 w-60 origin-top-right divide-y divide-gray-100 rounded-md bg-white dark:bg-dark-600 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+			>
+				<div className="px-1 py-1 ">
+					<MenuItem v-slot="active">
+						<NuxtLink
+							:class="` ${
+								active ? 'c-rose-500' : 'c-rose-900'
+							} group flex w-full items-center rounded-md px-2 py-2 text-sm space-x-2 `"
+							to="/about"
+						>
+							<span>About me</span></NuxtLink
+						>
+					</MenuItem>
+					<MenuItem v-slot="active">
+						<NuxtLink
+							:class="` ${
+								active ? 'c-rose-500' : 'c-rose-900'
+							} group flex w-full items-center rounded-md px-2 py-2 text-sm space-x-2 `"
+							to="/contact"
+						>
+							Contact me</NuxtLink
+						>
+					</MenuItem>
+					<MenuItem v-slot="active">
+						<NuxtLink
+							:class="` ${
+								active ? 'c-rose-500' : 'c-rose-900'
+							} group flex w-full items-center rounded-md px-2 py-2 text-sm space-x-2 `"
+							to="/projects"
+						>
+							Projects</NuxtLink
+						>
+					</MenuItem>
+
+					<MenuItem v-slot="active">
+						<button
+							:class="` ${
+								active ? 'c-rose-500' : 'c-rose-900'
+							} group flex w-full items-center rounded-md px-2 py-2 text-sm space-x-2 `"
+							@click="props.toggleDarkMode()"
+						>
+							<IconSun />
+							<IconMoon />
+						</button>
+					</MenuItem>
+				</div>
+			</MenuItems>
+		</transition>
+	</Menu>
+</template>
---
+++ components/header.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+	const NavItems = [
+		{ href: 'about', children: 'About me' },
+		{ href: 'contact', children: 'Contact me' },
+		{ href: 'projects', children: 'Projects' },
+	]
+
+	const dark = useDark()
+	const toggleDarkMode = () => (dark.value = !dark.value)
+</script>
+
+<template>
+	<header class="p-7 bg-white w-full dark:bg-transparent">
+		<nav
+			class="flex flex-row items-center justify-between container mx-auto max-w-xl font-sawarabi"
+		>
+			<h1 class="font-bold c-rose-500 text-xl" title="Gonzalo Molina - Portfolio">
+				<NuxtLink to="/">GM</NuxtLink>
+			</h1>
+			<ul class="xl:flex lg:flex flex-row hidden items-center space-x-6 text-lg">
+				<li
+					class="hover:c-rose-500 duration-100 ease-in-out"
+					v-for="{ href, children } in NavItems"
+				>
+					<NuxtLink :to="href">{{ children }}</NuxtLink>
+				</li>
+				<li>
+					<button
+						class="flex flex-row items-center justify-center h-8 w-8 c-rose-500"
+						@click="toggleDarkMode()"
+					>
+						<IconSun />
+						<IconMoon />
+					</button>
+				</li>
+			</ul>
+			<Dropdown :toggle-dark-mode="toggleDarkMode" />
+		</nav>
+	</header>
+</template>
